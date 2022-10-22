2f1b95a5592e118356fc53aafecb2b6f2e486b07
Khaled3bzaher <Khaled3bzaher@gmail.com>
2022-10-23 00:36:09 +0200

    Calculate First Row in NumberOfCustomers StoppingCriteria .

--- MultiQueueSimulation/Form1.cs
+++ MultiQueueSimulation/Form1.cs
@@ -19,10 +19,56 @@ namespace MultiQueueSimulation
             InitializeComponent();
         }
         SimulationSystem simSystem = new SimulationSystem();
+        public int calculateServiceForServer(int serverID,int RandomService)
+        {
+            foreach(TimeDistribution oneRow in simSystem.Servers[serverID].TimeDistribution)
+            {
+                if (RandomService >= oneRow.MinRange && RandomService <= oneRow.MaxRange)
+                    return oneRow.Time;
+            }
+            return 0;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             simSystem.readFromFile("G:/FCIS/Seventh Semester/Modeling & Simulation/Labs/Lab 2/Template_Students/MultiQueueSimulation/MultiQueueSimulation/TestCases/TestCase1.txt",simSystem);
-            
+            //Starting System
+            Random RandomInterArrival = new Random();
+            Random RandomService = new Random();
+            Random nextServer = new Random();
+            if (simSystem.StoppingCriteria == Enums.StoppingCriteria.NumberOfCustomers)
+            {
+                for(int customer = 0; customer < simSystem.StoppingNumber; customer++)
+                {
+                    SimulationCase oneCase = new SimulationCase();
+                    if (customer == 0)
+                    {
+                        oneCase.CustomerNumber = customer + 1;
+                        oneCase.RandomInterArrival = 1;
+                        oneCase.InterArrival = 0;
+                        oneCase.ArrivalTime = 0;
+                        oneCase.RandomService = RandomService.Next(1, 100);
+                        //Check Type Of System
+                        int serverID = 0;
+                        if (simSystem.SelectionMethod == Enums.SelectionMethod.Random)
+                        {
+                            serverID = nextServer.Next(1, simSystem.NumberOfServers);
+                        }
+                        oneCase.ServiceTime = calculateServiceForServer(serverID, oneCase.RandomService);
+                        oneCase.AssignedServer = simSystem.Servers[serverID];
+                        oneCase.StartTime = 0;
+                        oneCase.EndTime = oneCase.ServiceTime;
+                        oneCase.TimeInQueue = 0;
+                        simSystem.SimulationTable.Add(oneCase);
+                    }
+                    else
+                    {
+                        break;
+                        //Remove Break and Continue..
+                    }
+                }
+            }
+            string result = TestingManager.Test(simSystem, Constants.FileNames.TestCase1);
+            MessageBox.Show(result);
         }
     }
 }
